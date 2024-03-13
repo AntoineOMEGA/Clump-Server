137a64dcc2a1eb0a1bae61c67a5fb7d7ac3a4988
AntoineOMEGA <antoinemflorian@gmail.com>
2024-03-13 01:52:26 -0600

    Started on Event Creation

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,4 +1,6 @@
-//const Event = require('../models/eventModel');
+const Event = require('../models/eventModel');
+const Clump = require('../models/clumpModel');
+const Schedule = require('../models/scheduleModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
@@ -12,7 +14,7 @@ exports.getEvents = catchAsync(async (req, res, next) => {
   const features = new APIFeatures(Event.find(), req.query)
     .filter()
     .sort()
-    .limitFields()
+    .limitFields();
   const events = await features.query;
 
   res.status(200).json({
@@ -40,7 +42,6 @@ exports.getEvent = catchAsync(async (req, res, next) => {
 });
 
 exports.createEvent = catchAsync(async (req, res, next) => {
-
   const refreshToken = await Clump.findById(req.cookies.currentClumpID);
   oAuth2Client.setCredentials({
     refresh_token: refreshToken.googleToken,
@@ -48,8 +49,23 @@ exports.createEvent = catchAsync(async (req, res, next) => {
 
   const gCalendar = google.calendar({ version: 'v3', auth: oAuth2Client });
 
+  var event = {
+    summary: req.body.title,
+    location: req.body.location,
+    description: req.body.description,
+    start: {
+      dateTime: req.body.startDate + 'T' + req.body.startTime,
+      timeZone: 'America/Denver',
+    },
+    end: {
+      dateTime: req.body.startDate + 'T' + req.body.endTime,
+      timeZone: 'America/Denver',
+    },
+    recurrence: ['RRULE:FREQ=DAILY;COUNT=2'],
+  };
+/*
   let newEvent = {
-    title: req.body.summary,
+    title: req.body.title,
     googleEventID: req.body.id,
     description: req.body.description,
     location: req.body.location,
@@ -57,16 +73,23 @@ exports.createEvent = catchAsync(async (req, res, next) => {
     endDateTime: new Date(req.body.end.dateTime),
     scheduleID: req.body.scheduleID,
   };
+  */
 
-  gCalendar.events.insert();
-
-  //Needs Google Integration
-  res.status(201).json({
-    status: 'success',
-    data: {
-      event: newEvent,
-    },
+  let schedule = await Schedule.findById(req.body.scheduleID);
+  gCalendar.events.insert({
+    auth: oAuth2Client,
+    calendarId: schedule.googleCalendarID,
+    resource: event,
+  }, function (err, newEvent) {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        event: newEvent,
+      },
+    });
   });
+
+  
 });
 
 exports.updateEvent = catchAsync(async (req, res, next) => {
